405d57d2ef565c41f1bf9083e26ba7628e7fd6af
jaedie <jamesleehw@gmail.com>
2021-05-25 21:55:07 +0900

    changed firebase public id

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCDp8H3MwyvArGmLEs5szelb24fa9rnzpc",
-  authDomain: "clone-d8a5c.firebaseapp.com",
-  projectId: "clone-d8a5c",
-  storageBucket: "clone-d8a5c.appspot.com",
-  messagingSenderId: "402918534211",
-  appId: "1:402918534211:web:0324b28b190214e95ff8f6",
-  measurementId: "G-8D0VQB8PKS",
+  apiKey: "AIzaSyCW1l-etZl537IL7TFVFanL6hWe32JxUE0",
+  authDomain: "amzn-app-86035.firebaseapp.com",
+  projectId: "amzn-app-86035",
+  storageBucket: "amzn-app-86035.appspot.com",
+  messagingSenderId: "547872670149",
+  appId: "1:547872670149:web:c0a3761f54bb9ea0ff8bac",
+  measurementId: "G-WX2V7YFMTG",
 });
 
 const auth = firebase.auth();
